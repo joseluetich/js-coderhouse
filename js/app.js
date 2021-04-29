866e0bd666873d5d396b946d5263550b306ef478
joseluetich <josefinaluetich@gmail.com>
2021-04-28 23:49:40 -0300

    Html del proyecto

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//let age = prompt("Ingresa tu edad:");
+
+function allowed(param) {
+  if (param >= 18) {
+    alert("Bienvenido a Bebidas Zeppelin!");
+  } else {
+    alert("Necesitas ser mayor de edad para realizar compras");
+  }
+}
+
+//allowed(age);
